8b5d8cdadb2fb0badf0b18996253de35f55a3039
hz <1322271671@qq.com>
2023-07-06 15:23:21 +0800

    正式版1.0

---
+++ src/components/Header.vue
@@ -0,0 +1,133 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <h1>
+        <router-link to="#">艺品纷呈</router-link>
+      </h1>
+    </div>
+    <div class="search"><input type="search" name="" id="" placeholder="民间美术" /><button>搜索</button></div>
+    <nav class="nav"><span class="el-icon-s-operation"></span><span>网站导航</span></nav>
+    <h1 class="welcome">艺品纷呈欢迎你!</h1>
+    <h3>traditional culture</h3>
+    <div class="goNext" @click="playMusic1">continue</div>
+    <audio src="../assets/music/gsx.mp3" id="goNext" ref="goNext"></audio>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    playMusic1() {
+      // console.log(this.$refs.goNext) 还没写axios代码,等后面写了再改
+      if (this.$refs.goNext.played.length == 0 || this.$refs.goNext.paused) {
+        this.$refs.goNext.play()
+      } else {
+        this.$refs.goNext.pause()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  width: 10rem;
+  height: 9rem;
+  background: url(../assets/images/bg.jpg) no-repeat;
+  position: relative;
+  background-size: 100%;
+}
+
+/* logoSEO优化 */
+.logo a {
+  display: block;
+  background: url(../assets/images/logo.png) no-repeat;
+  background-size: 80%;
+  text-indent: -9999rem;
+  width: 1.3158rem;
+  height: 1.3158rem;
+}
+
+.header h1 {
+  font-size: 0.3947rem;
+}
+
+.header .welcome {
+  position: absolute;
+  left: 3.2895rem;
+  top: 1.3158rem;
+  color: #fff;
+}
+
+.header h3 {
+  position: absolute;
+  left: 3.8158rem;
+  top: 2.0658rem;
+  font-size: 0.1974rem;
+  color: #fff;
+}
+
+.header .goNext {
+  position: absolute;
+  left: 4.03rem;
+  top: 2.5rem;
+  width: 1.3158rem;
+  height: 0.3026rem;
+  text-align: center;
+  font-size: 0.1974rem;
+  border-radius: 0.1645rem;
+  background: #fff;
+  color: darkorange;
+}
+.nav {
+  overflow: hidden;
+  position: absolute;
+  top: 0.1645rem;
+  right: 0;
+  width: 152px;
+  height: 79.9976px;
+  color: #fff;
+  text-align: center;
+}
+
+.nav span {
+  position: absolute;
+  padding-left: 15.0024px;
+  padding-top: 10.0016px;
+  font-size: 13.9992px;
+  top: 10px;
+  right: 25px;
+}
+.nav .el-icon-s-operation {
+  position: absolute;
+  font-size: 0.1645rem;
+  top: 10px;
+  right: 100px;
+}
+.search {
+  position: absolute;
+  top: 0.858rem;
+  right: 2rem;
+}
+
+.header .search input {
+  width: 1.6447rem;
+  height: 0.2632rem;
+  border-radius: 0.1316rem;
+  border: 0.0066rem solid orange;
+  padding-left: 0.1rem;
+  font-size: 0.0789rem;
+}
+
+.header .search button {
+  width: 0.5rem;
+  height: 0.2632rem;
+  border-radius: 0.1316rem;
+  margin: -0.25rem;
+  border: 0.0066rem solid orange;
+  background-color: orange;
+  color: #fff;
+  font-size: 0.0658rem;
+  font-size: 0.0789rem;
+}
+</style>
---
+++ src/components/music.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="folkMusic">
+    <div class="content">
+      <h2>民间音乐</h2>
+      <div class="description">
+        民间音乐（Folk music），又称民俗音乐、民间短篇诗歌等，简称民谣、民歌、民乐，于国际民间音乐协会（IFMC）之解释定义为“经过口传过程发展起来的普罗大众音乐”，也就是该音乐散布过程，纯粹是由演奏者或音乐接收者记录教习，并亲自相传所得。
+        民间音乐指由广大人民群众在漫长历史过程中，通过口口相传而流传下来的音乐形式和音乐作品。它无论从使用的乐器、演奏的乐谱还是演奏形式，都有着极强的民族性和地域性，与当地的民俗习惯相溶合，与当地的民俗活动相结合。
+      </div>
+    </div>
+    <div class="videoBox">
+      <video src="" controls muted></video>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+#folkMusic {
+  position: relative;
+  width: 10rem;
+  height: 6.5789rem;
+  padding-top: 0.5rem;
+  background: url(../assets/images/folkMusic.png) no-repeat;
+}
+
+#folkMusic .content {
+  float: right;
+  padding-right: 0.6579rem;
+}
+
+#folkMusic .videoBox {
+  float: left;
+  border: 2px solid #ccc;
+  margin-left: 0.5rem;
+}
+</style>
